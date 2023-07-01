e102a55722df95b51f6052d9213e888588dcc4c3
Lewis Clarke <lclarke159@gmail.com>
2023-07-01 12:34:08 +0100

    Completed exercises 12 and 13_LC

---
+++ Exercise13/read_file_LC.py
@@ -0,0 +1,34 @@
+
+
+# 6 Use a loop to iterate over and print the contents of the file. Do not to include any blank lines in the output.
+
+with open('pelican_LC.txt') as x:
+    f = x.read()
+
+print("the type of data is ", type(f))
+
+print("")
+print("")
+
+print(f)
+
+print("")
+print("")
+
+# What data type is the output?
+print("the type of data is ", type(f))
+
+pelican_list = open("pelican_LC.txt").readlines()
+
+print("")
+print("")
+
+print(type(pelican_list))
+print("There are ", len(pelican_list), " items in this list")
+
+print("")
+print("")
+
+with open("pelican_LC.txt") as p:
+    for line in p:
+        print(line.strip())
